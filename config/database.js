7d544d747375a5d710e6a9189d2421fd23c1bf4c
Sattwik <sattwikroy@hotmail.com>
2021-09-21 17:09:54 +0530

    git upload

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://sattwik:s5678@cluster0.ade22.mongodb.net/GrowGreen?retryWrites=true&w=majority',
+    database: "", //Add MongoDB URL HERE
     secret: '--*=]ywq;lkjhgfdsaP[-==,M0'
 }
